2b9103ffaaf66773f74165d3f4dd3b0d51754c19
shiliantai <awaketai@163.com>
2025-12-24 09:55:02 +0800

    feat: multi agent

---
+++ 35_multi_agent.py
@@ -0,0 +1,9 @@
+# Multi-agent 系统用来协调各种专门的组件来处理复杂的流程。
+# https://docs.langchain.com/oss/python/langchain/multi-agent/skills
+# 但是并不是每项复杂的任务都需要这种模式，只要具备恰当(有时候是动态)的工具，一个单一的agent也可实现相似的效果
+#
+#
+# Skills：在 skill 架构中，特定的能力被封装为 "skill" ，以增强 agent 的行为。skill是主要的 prompt-driven 专业模块
+# agent 可按需调用
+#
+#
