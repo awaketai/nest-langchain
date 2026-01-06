50ca3a2862b881d6373ae2f103cee34e7f6590d6
shiliantai <awaketai@163.com>
2026-01-06 10:46:09 +0800

    feat: complete langgraph 2

--- 37_langgraph2.py
+++ 37_langgraph2.py
@@ -1,10 +1,12 @@
 import operator
 import os
+from typing import Literal
 
 from dotenv import load_dotenv
 from langchain.tools import tool
-from langchain_core.messages import AnyMessage, SystemMessage, ToolMessage
+from langchain_core.messages import AnyMessage, HumanMessage, SystemMessage, ToolMessage
 from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START, StateGraph
 from pydantic import SecretStr
 from typing_extensions import Annotated, TypedDict
 
@@ -65,7 +67,7 @@ def divide(a: int, b: int) -> float:
 
 tools = [add, multiply, divide]
 tools_by_name = {tool.name: tool for tool in tools}
-model_with_tools = model.bind(tools=tools)
+model_with_tools = model.bind_tools(tools)
 
 
 # 2.Define state
@@ -92,13 +94,11 @@ def llm_call(state: dict):
                 + state["messages"]
             )
         ],
-        "llm_calls": state["llm_calls", 0] + 1,
+        "llm_calls": state.get("llm_calls", 0) + 1,
     }
 
 
 # 4.定义tool节点
-
-
 def tool_node(state: dict):
     """
     Performs the tool call.
@@ -110,3 +110,41 @@ def tool_node(state: dict):
         result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
 
     return {"messages": result}
+
+
+# 5.定义结束逻辑
+def should_continue(state: MessagesState) -> Literal["tool_name", END]:
+    """
+    Decide if we should continue the loop or sop based upon whether the LLM made a tool call.
+    """
+    messages = state["messages"]
+    last_message = messages[-1]
+    # 如果LLM 要进行工具调用
+    if last_message.tool_calls:
+        return "tool_node"
+
+    return END
+
+
+# 6.编译agent
+# 构建workflow
+agent_builder = StateGraph(MessagesState)
+# 添加节点
+agent_builder.add_node("llm_call", llm_call)
+agent_builder.add_node("tool_node", tool_node)
+# 添加边界，连接节点
+agent_builder.add_edge(START, "llm_call")
+agent_builder.add_conditional_edges("llm_call", should_continue, ["tool_node", END])
+agent_builder.add_edge("tool_node", END)
+# compile
+agent = agent_builder.compile()
+# show agent
+
+# from IPython.display import Image, display
+# display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
+
+# 调用
+messages = [HumanMessage(content="add 3 and 4.")]
+messages = agent.invoke({"messages": messages})
+for m in messages["messages"]:
+    m.pretty_print()
