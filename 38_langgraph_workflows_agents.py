fd1acf9ed3b3aafa83df50302e4765bfb49c5ea8
shiliantai <awaketai@163.com>
2026-01-08 14:08:47 +0800

    feat: complete graph workflow

--- 38_langgraph_workflows_agents.py
+++ 38_langgraph_workflows_agents.py
@@ -7,6 +7,8 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field, SecretStr
+from typing_extensions import TypedDict
+from langgraph.graph import END, START, StateGraph
 
 load_dotenv()
 
@@ -20,6 +22,7 @@ llm = ChatOpenAI(
     base_url=OPEN_API_URL,
     api_key=SecretStr(OPEN_API_KEY),
     temperature=0,
+    model="gpt-3.5-turbo"
 )
 
 
@@ -46,6 +49,76 @@ def multiply(a: int, b: int) -> int:
 llm_with_tools = llm.bind_tools([multiply])
 
 # 执行调用
-msg = llm_with_tools.invoke("What is 2 times 3?")
+msg = llm_with_tools.invoke("What is 2 multiply 3?")
 # 获取工具调用信息
-msg.tool_calls
+response = msg.tool_calls
+print(response)
+
+# Graph State
+class State(TypedDict):
+    topic:str
+    joke: str
+    improved_joke: str
+    final_joke:str
+
+# Nodes
+def general_joke(state: State):
+    """
+    首次调用
+    """
+    msg = llm.invoke(f"Write a short joke about {state['topic']}")
+    return {'joke':msg.content}
+
+def check_punchline(state: State):
+    """
+    Gate funciton to check if the joke has a punchline
+    """
+    if "?" in state['joke'] or "!" in state['joke']:
+        return "Pass"
+    return "Fail"
+
+def improve_joke(state: State):
+    """
+    Second LLM call to improve the joke
+    """
+    msg = llm.invoke(f"Make this joke funnier by adding wordplay: {state['joke']}")
+    return {"improved_joke": msg.content}
+
+def polish_joke(state: State):
+    """
+    Third LLM call for final polish
+    """
+    msg = llm.invoke(f"Add a surprising twist to this joke: {state['improved_joke']}")
+    return {"final_joke": msg.content}
+
+# 构建工作流
+workflow = StateGraph(State)
+
+# 添加节点
+workflow.add_node("generate_joke",general_joke)
+workflow.add_node("improve_joke",improve_joke)
+workflow.add_node("polish_joke",polish_joke)
+
+# 添加边界
+workflow.add_edge(START,"generate_joke")
+workflow.add_conditional_edges("generate_joke",check_punchline,{"Fail":"improve_joke","Pass":END})
+workflow.add_edge("improve_joke","polish_joke")
+workflow.add_edge("polish_joke",END)
+
+# 编译
+chain = workflow.compile()
+
+state = chain.invoke({"topic":"dogs"})
+
+print("initial joke:",state['joke'])
+print("\n--- --- ---\n")
+if "improved_joke" in state:
+    print("Improved joke:")
+    print(state["improved_joke"])
+    print("\n--- --- ---\n")
+
+    print("Final joke:")
+    print(state["final_joke"])
+else:
+    print("Final joke:")
+    print(state["joke"])
